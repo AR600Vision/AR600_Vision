ca8db22c9b47a742811f1c004e5bd21ce889a48d
Garrus <Markov Alexey>
2016-07-16 20:49:32 +0300

    Сделал нормали и поворот облака, чтобы оси были направлены нормально

--- src/steps_controller.cpp
+++ src/steps_controller.cpp
@@ -13,25 +13,38 @@
 
 #include <ros/ros.h>
 
+
+#include <cmath>
+
 // PCL specific includes
 #include <pcl/filters/voxel_grid.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
 #include <pcl/visualization/pcl_visualizer.h>
-
+#include <pcl/features/normal_3d.h>
+#include <pcl/common/transforms.h>
 
 const char point_cloud_topic[] = "camera/depth/points";
 const char point_cloud_name[] = "cloud";
 const float downsample_leaf_size =  0.05;
 
+#define POINT_TYPE pcl::PointXYZ
+
 boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;                            //Визуализатор
 pcl::VoxelGrid<pcl::PCLPointCloud2> sor;                                                //Для уменьшения плотности точек
+pcl::NormalEstimation<POINT_TYPE, pcl::Normal> ne;
 
-boost::shared_ptr<pcl::visualization::PCLVisualizer> create_viz();                      //Создание вьювера
-void pointcloud_callback(const sensor_msgs::PointCloud2ConstPtr& input);                //Принимает облако точек
-pcl::PCLPointCloud2::Ptr downsample_cloud(pcl::PCLPointCloud2::Ptr cloud);              //Уменьшает количество точек в облаке
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> create_viz();                          //Создание вьювера
+void pointcloud_callback(const sensor_msgs::PointCloud2ConstPtr& input);                    //Принимает облако точек
+pcl::PCLPointCloud2::Ptr downsample_cloud(pcl::PCLPointCloud2::Ptr cloud);                  //Уменьшает количество точек в облаке
+pcl::PointCloud<pcl::Normal>::Ptr calulate_normals(pcl::PointCloud<POINT_TYPE>::Ptr cloud); //Расчет нормалей
+pcl::PointCloud<POINT_TYPE>::Ptr rotate_cloud(pcl::PointCloud<POINT_TYPE>::Ptr cloud);      //Поворачивает облако
+
+double deg_to_rad(float deg);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(int argc, char** argv)
 {
@@ -82,19 +95,28 @@ void pointcloud_callback(const sensor_msgs::PointCloud2ConstPtr& input)
         return;
     }
 
-    //Преобразование в pcl::PointCloud<T>
+    //Преобразование в PCL
     pcl::PCLPointCloud2 pointCloud2;
     pcl_conversions::toPCL(*input, pointCloud2);
 
     //Уменьшение размера
     pcl::PCLPointCloud2::Ptr downsampled_cloud = downsample_cloud(boost::make_shared<pcl::PCLPointCloud2>(pointCloud2));
 
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    //Преобразование в pcl::PointCloud<T>
+    pcl::PointCloud<POINT_TYPE>::Ptr cloud(new pcl::PointCloud<POINT_TYPE>);
     pcl::fromPCLPointCloud2(*downsampled_cloud,*cloud);
 
+    //Поворот
+    pcl::PointCloud<POINT_TYPE>::Ptr transformed_cloud = rotate_cloud(cloud);
+
+    //Расчет нормалей
+    //pcl::PointCloud<pcl::Normal>::Ptr normals = calulate_normals(transformed_cloud);
+
     //Обновляем облако в визуализаторе
-    viewer->removePointCloud();
-    viewer->addPointCloud(cloud,point_cloud_name);
+    viewer->removeAllPointClouds(0);
+    viewer->addPointCloud(transformed_cloud,point_cloud_name);
+    //viewer->addPointCloudNormals<POINT_TYPE, pcl::Normal>(transformed_cloud, normals, 1, 0.015, "normals", 0);
+
 
     //viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
 }
@@ -110,4 +132,45 @@ pcl::PCLPointCloud2::Ptr downsample_cloud(pcl::PCLPointCloud2::Ptr cloud)
     sor.filter(*cloud_filtered);
 
     return cloud_filtered;
+}
+
+pcl::PointCloud<pcl::Normal>::Ptr calulate_normals(pcl::PointCloud<POINT_TYPE>::Ptr cloud)
+{
+    ne.setInputCloud (cloud);
+    pcl::search::KdTree<POINT_TYPE>::Ptr tree (new pcl::search::KdTree<POINT_TYPE> ());
+    ne.setSearchMethod (tree);
+    pcl::PointCloud<pcl::Normal>::Ptr cloud_normals1 (new pcl::PointCloud<pcl::Normal>);
+    ne.setRadiusSearch (0.05);
+    ne.compute (*cloud_normals1);
+
+    return cloud_normals1;
+}
+
+/**
+ * Вращает облако  и приводит его к нормальному положению:
+ *   X - вперед
+ *   Y - влево
+ *   Z - ввврх
+ *
+ * ВНИМАНИЕ
+ * только для непосредственных данных с кинекта, данные
+ * RTABMap и других библиотек имеют другую ориентацию
+ */
+pcl::PointCloud<POINT_TYPE>::Ptr rotate_cloud(pcl::PointCloud<POINT_TYPE>::Ptr cloud)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr transformed_cloud (new pcl::PointCloud<pcl::PointXYZ> ());
+    Eigen::Affine3f transforms = Eigen::Affine3f::Identity();
+    transforms.rotate(Eigen::AngleAxis<float>(deg_to_rad(180), Eigen::Vector3f::UnitZ()));
+    transforms.rotate(Eigen::AngleAxis<float>(deg_to_rad(-90), Eigen::Vector3f::UnitY()));
+    transforms.rotate(Eigen::AngleAxis<float>(deg_to_rad(90), Eigen::Vector3f::UnitZ()));
+    pcl::transformPointCloud(*cloud, *transformed_cloud, transforms);
+
+    return transformed_cloud;
+
+}
+
+//Преобразует градусы в радианы
+double deg_to_rad(float deg)
+{
+    return deg / 180.0 * M_PI;
 }
