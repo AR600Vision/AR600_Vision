01f1fe538fd4ed542f3d53fbec55ea91e7e23811
Garrus <Markov Alexey>
2016-07-16 18:37:57 +0300

    Добавил фильтр, уменьшающий число точек

--- src/steps_controller.cpp
+++ src/steps_controller.cpp
@@ -14,6 +14,7 @@
 #include <ros/ros.h>
 
 // PCL specific includes
+#include <pcl/filters/voxel_grid.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
@@ -21,10 +22,45 @@
 #include <pcl/visualization/pcl_visualizer.h>
 
 
-#define POINT_CLOUD_TOPIC "camera/depth/points"
-#define POINT_CLOUD_NAME "cloud"
+const char point_cloud_topic[] = "camera/depth/points";
+const char point_cloud_name[] = "cloud";
+const float downsample_leaf_size =  0.05;
 
-boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;// (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer;                            //Визуализатор
+pcl::VoxelGrid<pcl::PCLPointCloud2> sor;                                                //Для уменьшения плотности точек
+
+boost::shared_ptr<pcl::visualization::PCLVisualizer> create_viz();                      //Создание вьювера
+void pointcloud_callback(const sensor_msgs::PointCloud2ConstPtr& input);                //Принимает облако точек
+pcl::PCLPointCloud2::Ptr downsample_cloud(pcl::PCLPointCloud2::Ptr cloud);              //Уменьшает количество точек в облаке
+
+int main(int argc, char** argv)
+{
+    //Инициализация ноды ROS
+    ros::init(argc, argv, "steps_controller");
+    ros::NodeHandle n;
+
+    ROS_INFO("%s", "AR-600 steps controller started");
+
+    //Создаем визуализатор
+    viewer = create_viz();
+
+    //Подписываемся на топик с облаком точек
+    ros::Subscriber sub = n.subscribe(point_cloud_topic ,1, pointcloud_callback);
+
+    //ros::spin ();
+
+    while (!viewer->wasStopped ())
+    {
+        viewer->spinOnce (100);
+        ros::spinOnce();
+        boost::this_thread::sleep (boost::posix_time::microseconds (1000));
+    }
+
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //Создание вьювера
 boost::shared_ptr<pcl::visualization::PCLVisualizer> create_viz()
@@ -49,40 +85,29 @@ void pointcloud_callback(const sensor_msgs::PointCloud2ConstPtr& input)
     //Преобразование в pcl::PointCloud<T>
     pcl::PCLPointCloud2 pointCloud2;
     pcl_conversions::toPCL(*input, pointCloud2);
+
+    //Уменьшение размера
+    pcl::PCLPointCloud2::Ptr downsampled_cloud = downsample_cloud(boost::make_shared<pcl::PCLPointCloud2>(pointCloud2));
+
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    pcl::fromPCLPointCloud2(pointCloud2,*cloud);
+    pcl::fromPCLPointCloud2(*downsampled_cloud,*cloud);
 
     //Обновляем облако в визуализаторе
     viewer->removePointCloud();
-    viewer->addPointCloud(cloud,POINT_CLOUD_NAME);
+    viewer->addPointCloud(cloud,point_cloud_name);
 
     //viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
 }
 
 
-int main(int argc, char** argv)
+//Уменьшает количество точек в облаке
+pcl::PCLPointCloud2::Ptr downsample_cloud(pcl::PCLPointCloud2::Ptr cloud)
 {
-    //Инициализация ноды ROS
-    ros::init(argc, argv, "steps_controller");
-    ros::NodeHandle n;
+    pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
 
-    ROS_INFO("%s", "AR-600 steps controller started");
+    sor.setInputCloud(cloud);
+    sor.setLeafSize(downsample_leaf_size,downsample_leaf_size, downsample_leaf_size);
+    sor.filter(*cloud_filtered);
 
-    //Создаем визуализатор
-    viewer = create_viz();
-
-    //Подписываемся на топик с облаком точек
-    ros::Subscriber sub = n.subscribe(POINT_CLOUD_TOPIC ,1, pointcloud_callback);
-
-    //ros::spin ();
-
-    while (!viewer->wasStopped ())
-    {
-        viewer->spinOnce (100);
-        ros::spinOnce();
-        boost::this_thread::sleep (boost::posix_time::microseconds (1000));
-    }
-
-
-    return 0;
+    return cloud_filtered;
 }
